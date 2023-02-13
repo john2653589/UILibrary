d623bd7a8643de2f7d3ccf70cfd9828d6470d005
Rugal Tu <john2653589@gmail.com>
2023-02-13 13:45:42 +0800

    Rugal Update

--- UILibrary/Library/UILibraryCore/Model/ApiModel.cs
+++ UILibrary/Library/UILibraryCore/Model/ApiModel.cs
@@ -2,6 +2,7 @@
 {
     public class ApiGroup
     {
+        public string GroupDescribe { get; set; }
         public string GroupName { get; set; }
         public List<Api> Apis { get; set; } = new List<Api>();
     }
@@ -9,7 +10,7 @@
     {
         public string ApiKey { get; set; }
         public string Url { get; set; }
-        public string Describe { get; set; }
+        public string ApiDescribe { get; set; }
     }
 
     public class DeleteApiModel
