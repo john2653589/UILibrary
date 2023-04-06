46c246df1e555d506037ebebe88ae69d99e9a4a7
Rugal Tu <john2653589@gmail.com>
2023-04-06 17:50:25 +0800

    Rugal Update

--- UILibrary/wwwroot/js/test.js
+++ UILibrary/wwwroot/js/test.js
@@ -4,31 +4,69 @@
 
     let Url = Api.ExportData.ExportData;
 
-    Api.ComponentTest
-
     const VcConfig = {
         VcName: 'TestComponent',
         Api: {
-            //TestGet: {
-            //    Url: ApiGroup.TestGet,
-            //    MethodType: 'GET',
-            //    Bind: {
-            //        'm:text': 'Title,Id',
-            //    },
-            //},
-            TestPost: {
+            //ApiKey = StorageKey
+            TestPost: { //Url、Type、Bind
                 Url: ApiGroup.TestPost,
-                MethodType: 'POST',
+                Type: 'POST',
                 Bind: {
-                    ForLabel : 'm:text, c:Item.Title',
-                }
+                    //vc-col
+                    Lbl: {
+                        Mode: 'text',
+                        //From Source
+                        From: 'Title', //Default From = TestPost.Title
+                    },
+                    Lbl4: {
+                        Mode: 'text',
+                        From: 'TestGet.Title',
+                    },
+                    Lbl2: {
+                        Mode: 'input',
+                        //Default From = vc-col
+                        //From : TestPost.Lbl2
+                    },
+                    Lbl3: 'm:input; f:Title',
+                    Sel4: {
+                        Mode: 'select',
+                        Opt: 'Opt5',
+                        Display: 'CountyName', // Item.CountyName
+                        Value: 'CountyId', // Item.CountyId
+                        To: 'Result',
+                        //From: 'TestPost' ; Not TestPost.Sel4
+                    },
+                    Sel42: 'm:select; opt:opt5; d:CountyName; v:CountyId; to:Result',
+                    Sel5: {
+                        Mode: 'select',
+                        Opt: 'Opt6',
+                        Value: 'CountyId',
+                        From: '-html',
+                    },
+                    Sel6: 'm:select; opt:Opt6; v:CountyId; f:-html',
+                    Array7: {
+                        Mode: 'for',
+                        //From: 'TestPost' ; Not TestPost.Array7
+                    },
+                    ForText: { //Label
+                        Mode: 'text',
+                        From: 'Item.ForText',
+                    },
+                    ForInput: { //Input
+                        Mode: 'input',
+                        From: 'Item.ForInput',
+                    },
+                },
+                AutoBind: { // vc-auto="input"; vc-auto="text"
+                    //From: 'TestPost',
+                    //To: 'TestPost',
+                },
             },
-            TestSelect: {
-                Url: ApiGroup.TestSelect,
-                MethodType: 'GET',
-                Bind: {
-                    TargetSelect: 'c:., m:select, opt:TestOption', // 預設 c:.
-                    'm:select': 'TargetSelectA, opt:TestOption, d:Key, v:Value'
+        },
+        Bind: {
+            StorageKey: {
+                AutoBind: {
+                    To: 'Create',
                 }
             }
         },
@@ -39,12 +77,10 @@
         .Done();
 
     Model
-        //.ApiCall('TestGet')
+        //.ApiCall('TestGet')e4
         //.ApiCall('TestPost', {
         //    Id: '111',
         //    Title: '222'
         //})
         .Init();
-
-
 });
